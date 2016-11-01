58d779afcad11374debc08bec41de190b2118b18
facundo.mengoni <facundo.mengoni@globallogic.com>
2016-11-01 14:20:04 -0300

    New mapper test

--- app/src/test/java/com/globallogic/codingdojo/test/TestFeedsMapper.java
+++ app/src/test/java/com/globallogic/codingdojo/test/TestFeedsMapper.java
@@ -1,11 +1,11 @@
 package com.globallogic.codingdojo.test;
 
-import com.globallogic.codingdojo.domain.mappers.Transformable;
-import com.globallogic.codingdojo.domain.model.RSS;
+import com.globallogic.codingdojo.data.dto.RssDTO;
 import com.globallogic.codingdojo.di.component.DaggerTestPresenterComponent;
 import com.globallogic.codingdojo.di.module.TestPresenterModule;
+import com.globallogic.codingdojo.domain.mappers.Transformable;
+import com.globallogic.codingdojo.domain.model.RSS;
 import com.globallogic.codingdojo.mock.RSSMockFactory;
-import com.globallogic.codingdojo.data.dto.RssDTO;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -41,5 +41,14 @@ public class TestFeedsMapper {
         assertNotNull(rss);
         assertNotNull(rss.getItems());
         assertEquals(2, rss.getItems().size());
+    }
+
+    @Test
+    public void testNoDataTransformRss() {
+        RssDTO rssWithTwoItems = mockFactory.getRssWithNoData();
+        RSS rss = mapper.transform(rssWithTwoItems);
+        assertNotNull(rss);
+        assertNotNull(rss.getItems());
+        assertEquals(0, rss.getItems().size());
     }
 }
